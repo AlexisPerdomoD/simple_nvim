0d8329ce2b052f47d3dd0b0264c920a0f819de8d
alexisperdomod <dropdibemol@gmail.com>
2024-07-17 00:14:58 -0500

    fix

--- lua/plugins/linters.lua
+++ lua/plugins/linters.lua
@@ -66,7 +66,7 @@ return {
             end,
         })
 
-        vim.keymap.set("n", "<leader>l", function()
+        vim.keymap.set("n", ".l", function()
             lint.try_lint()
         end, { desc = "Trigger linting for current file" })
     end,
