f468b570116c3d569952264e44905c020b2ad4c6
cuji <alexmetal66@gmail.com>
2025-04-07 09:05:22 -0500

    GENERAL: keybinding and color_bg_setup util

---
+++ lua/utils/bg_color_setuper.lua
@@ -0,0 +1,26 @@
+---@param disable_bg_color boolean
+---@param disable_float_bg_color boolean
+local function bg_setuper(disable_bg_color, disable_float_bg_color)
+    if disable_bg_color then
+        vim.cmd [[hi Normal guibg=NONE]]
+    end
+    if disable_float_bg_color then
+        vim.cmd [[hi FloatBorder guibg=NONE]]
+    end
+    return nil
+end
+
+Bg_transparent = false
+
+local function toggle_bg()
+    Bg_transparent = not Bg_transparent
+    print(Bg_transparent)
+    bg_setuper(Bg_transparent, Bg_transparent)
+end
+
+
+return {
+    bg_setuper = bg_setuper,
+    toggle_bg = toggle_bg,
+    Bg_transparent = Bg_transparent
+}
