2f970d7dfc3967006ac77d85c34c5e9bceb0dc77
alexisperdomod <dropdibemol@gmail.com>
2025-04-26 07:36:01 -0500

    feat: implement new status bar

--- lua/plugins/status_bar.lua
+++ lua/plugins/status_bar.lua
@@ -1,147 +1,47 @@
 return {
-    'glepnir/galaxyline.nvim',
-    branch = 'main',
-    dependencies = { 'nvim-tree/nvim-web-devicons', opt = true },
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-        -- Configuración de galaxyline
-        local gl = require('galaxyline')
-        local condition = require('galaxyline.condition')
-        local gls = gl.section
-
-        -- Definir colores genéricos
-        local colors = {
-            bg = '#121218',        -- Background color
-            fg = '#D8DEE9',        -- Foreground color
-            black = '#2E3440',     -- Black
-            red = '#BF616A',       -- Red
-            green = '#A3BE8C',     -- Green (Git color)
-            yellow = '#EBCB8B',    -- Yellow
-            blue = '#81A1C1',      -- Blue
-            magenta = '#B48EAD',   -- Magenta
-            cyan = '#88C0D0',      -- Cyan
-            white = '#ECEFF4',     -- White
-            grey = '#4C566A',      -- Grey
-            darkgrey = '#3B4252',  -- Dark Grey
-            solidgrey = '#434C5E', -- Solid Grey (Between almost black and silver)
-        }
-
-        gl.short_line_list = { 'NvimTree', 'vista', 'dbui', 'packer' }
-
-        -- Left side of the statusline
-        -- Mode indicator
-        gls.left[2] = {
-            ViMode = {
-                provider = function()
-                    local mode_color = {
-                        n = colors.green,
-                        i = colors.blue,
-                        v = colors.magenta,
-                        [''] = colors.magenta,
-                        V = colors.magenta,
-                        c = colors.red,
-                        no = colors.red,
-                        s = colors.orange,
-                        S = colors.orange,
-                        [''] = colors.orange,
-                        ic = colors.yellow,
-                        R = colors.violet,
-                        Rv = colors.violet,
-                        cv = colors.red,
-                        ce = colors.red,
-                        r = colors.cyan,
-                        rm = colors.cyan,
-                        ['r?'] = colors.cyan,
-                        ['!'] = colors.red,
-                        t = colors.red,
-                    }
-                    vim.api.nvim_command('hi GalaxyViMode guifg=' .. mode_color[vim.fn.mode()])
-                    return '  💀  '
-                end,
-                highlight = { colors.red, colors.bg, 'bold' },
+        require('lualine').setup {
+            options = {
+                icons_enabled = true,
+                theme = 'auto',
+                component_separators = { left = '', right = '' },
+                section_separators = { left = '', right = '' },
+                disabled_filetypes = {
+                    statusline = {},
+                    winbar = {},
+                },
+                ignore_focus = {},
+                always_divide_middle = true,
+                always_show_tabline = true,
+                globalstatus = false,
+                refresh = {
+                    statusline = 100,
+                    tabline = 100,
+                    winbar = 100,
+                }
             },
-        }
-
-        gls.left[3] = {
-            GitBranch = {
-                provider = 'GitBranch',
-                condition = condition.check_git_workspace,
-                highlight = { colors.green, colors.bg, 'bold' },
-                separator = ' ',
-                separator_highlight = { 'NONE', colors.bg },
-            }
-        }
-        gls.left[4] = {
-            FileName = {
-                provider = 'FileName',
-                highlight = { colors.darkgrey, colors.bg, 'bold' },
-                separator = ' ',
-                separator_highlight = { 'NONE', colors.bg },
-            }
-        }
-
-        -- Right side of the statusline
-        gls.right[1] = {
-            FileEncode = {
-                provider = 'FileEncode',
-                condition = condition.hide_in_width,
-                highlight = { colors.grey, colors.bg, 'bold' },
-                separator = ' ',
-                separator_highlight = { 'NONE', colors.bg },
-            }
-        }
-
-        gls.right[2] = {
-            FileFormat = {
-                provider = 'FileFormat',
-                condition = condition.hide_in_width,
-                highlight = { colors.green, colors.bg, 'bold' },
-                separator = ' ',
-                separator_highlight = { 'NONE', colors.bg },
-            }
-        }
-
-        gls.right[3] = {
-            LineInfo = {
-                provider = 'LineColumn',
-                highlight = { colors.darkgrey, colors.bg },
-                separator = ' ',
-                separator_highlight = { 'NONE', colors.bg },
+            sections = {
+                lualine_a = { 'mode' },
+                lualine_b = { 'branch', 'diff', 'diagnostics' },
+                lualine_c = { 'filename' },
+                lualine_x = { 'encoding', 'fileformat', 'filetype' },
+                lualine_y = { 'progress' },
+                lualine_z = { 'location' }
             },
+            inactive_sections = {
+                lualine_a = {},
+                lualine_b = {},
+                lualine_c = { 'filename' },
+                lualine_x = { 'location' },
+                lualine_y = {},
+                lualine_z = {}
+            },
+            tabline = {},
+            winbar = {},
+            inactive_winbar = {},
+            extensions = {}
         }
-
-        gls.right[4] = {
-            PerCent = {
-                provider = 'LinePercent',
-                highlight = { colors.fg, colors.bg, 'bold' },
-
-            }
-        }
-
-
-        -- Disable mode indicator on short lines
-        gls.short_line_left[1] = {
-            BufferType = {
-                provider = 'FileTypeName',
-                separator = ' ',
-                separator_highlight = { 'NONE', colors.bg },
-                highlight = { colors.fg, colors.bg }
-            }
-        }
-
-        gls.short_line_left[2] = {
-            SFileName = {
-                provider = 'SFileName',
-                condition = condition.buffer_not_empty,
-                highlight = { colors.fg, colors.bg }
-            }
-        }
-
-        -- gls.short_line_right[0] = {
-        --   BufferIcon = {
-        --     provider = 'BufferIcon',
-        --     highlight = { colors.fg, colors.bg }
-        --   }
-        -- }
-        vim.opt.laststatus = 3 -- Usa una sola barra de estado global
     end
 }
