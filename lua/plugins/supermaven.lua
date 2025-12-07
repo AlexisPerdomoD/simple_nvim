b422b207aa64a6ad2a35711bf653d296c931a9d9
cuji <alexmetal66@gmail.com>
2025-12-06 21:28:35 -0500

    update: lazy load for supermaven support

--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -1,30 +1,31 @@
-return {
-    'supermaven-inc/supermaven-nvim',
-    lazy = true,
-    config = function()
-        local sm = require 'supermaven-nvim'
-        sm.setup {
-            keymaps = {
-                accept_suggestion = '<C-a>',
-                clear_suggestion = '<C-]>',
-                accept_word = '<C-w>',
-            },
-            ignore_filetypes = { cpp = true }, -- or { "cpp", }
-            color = {
-                -- suggestion_color = "#ffffff",
-                -- cterm = 244,
-            },
-            log_level = 'off', -- set to "off" to disable logging completely
-            disable_inline_completion = false, -- disables inline completion for use with cmp
-            disable_keymaps = false, -- disables built in keymaps for more manual control
-            condition = function()
-                return false -- condition to check for starting supermaven, `true` means to start supermaven when the condition is true.
-            end,
-        }
-        vim.cmd 'SupermavenStop'
-        vim.cmd 'SupermavenStart'
-    end,
-}
+local M = { 'supermaven-inc/supermaven-nvim' }
+M.lazy = true
+
+M.config = function()
+    local sm = require 'supermaven-nvim'
+    sm.setup {
+        keymaps = {
+            accept_suggestion = '<C-a>',
+            clear_suggestion = '<C-]>',
+            accept_word = '<C-w>',
+        },
+        ignore_filetypes = { cpp = true }, -- or { "cpp", }
+        color = {
+            -- suggestion_color = "#ffffff",
+            -- cterm = 244,
+        },
+        log_level = 'off', -- set to "off" to disable logging completely
+        disable_inline_completion = false, -- disables inline completion for use with cmp
+        disable_keymaps = false, -- disables built in keymaps for more manual control
+        condition = function()
+            return false -- condition to check for starting supermaven, `true` means to start supermaven when the condition is true.
+        end,
+    }
+    vim.cmd 'SupermavenStop'
+    vim.cmd 'SupermavenStart'
+end
+
+return M
 -- :SupermavenStart    start supermaven-nvim
 -- :SupermavenStop     stop supermaven-nvim
 -- :SupermavenRestart  restart supermaven-nvim
