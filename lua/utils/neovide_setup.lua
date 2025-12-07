d84790b2b11b0156fc25585f59a8c6c58456768c
cuji <alexmetal66@gmail.com>
2025-12-06 21:25:04 -0500

    feat: improve neovide setup

--- lua/utils/neovide_setup.lua
+++ lua/utils/neovide_setup.lua
@@ -1,20 +1,20 @@
---- @param delta number
-function AdjustTransparency(delta) vim.g.neovide_opacity = math.min(1.0, math.max(0.1, vim.g.neovide_opacity + delta)) end
 local M = {}
+---
+--- @param delta number
+function M:set_transparency(delta)
+    local currentOpacityPercentage = vim.g.neovide_opacity + delta
+    vim.g.neovide_opacity = math.min(1.0, math.max(0.1, currentOpacityPercentage))
+    self.last_notify = vim.notify('Opacity set to ' .. vim.g.neovide_opacity * 100 .. '%', vim.log.levels.INFO)
+end
 
-M.init = function()
-    vim.g.neovide_cursor_vfx_mode = 'railgun'
-    vim.o.guifont = 'FantasqueSansM Nerd Font:h9'
-    -- vim.o.guifont = 'MartianMono Nerd Font:h8'
-    vim.g.neovide_text_gamma = 1
-    vim.g.neovide_text_contrast = 1
-    vim.g.neovide_input_use_logo = true
-    -- vim.g.neovide_refresh_rate = 165
-    vim.g.neovide_refresh_rate_idle = 15
-    vim.g.neovide_opacity = 0.8
-    vim.g.transparency = 1
-    vim.opt.linespace = 2
-    vim.g.neovide_floating_corner_radius = 0.5
+--- set environment variables for neovide
+function M.set_env()
+    local java_home = os.getenv 'HOME' .. '/.sdkman/candidates/java/current'
+    vim.fn.setenv('JAVA_HOME', java_home)
+    vim.fn.setenv('PATH', java_home .. '/bin:' .. os.getenv 'PATH')
+end
+
+function M.set_keymaps()
     vim.api.nvim_set_keymap('n', '<sc-v>', 'l"+P', { noremap = true })
     vim.api.nvim_set_keymap('c', '<sc-v>', '<C-o>l<C-o>"+<C-o>P<C-o>l', { noremap = true })
     vim.api.nvim_set_keymap('t', '<sc-v>', '<Cd\\><C-n>"+Pi', { noremap = true })
@@ -35,9 +35,27 @@ M.init = function()
         function() vim.g.neovide_scale_factor = (vim.g.neovide_scale_factor or 1.0) - 0.05 end,
         { noremap = true }
     )
+end
 
-    vim.keymap.set('n', '<C-)>', function() AdjustTransparency(0.05) end)
-    vim.keymap.set('n', '<C-(>', function() AdjustTransparency(-0.05) end)
+function M:init()
+    vim.g.neovide_cursor_vfx_mode = 'railgun'
+    vim.o.guifont = 'FantasqueSansM Nerd Font:h12'
+    -- vim.o.guifont = 'MartianMono Nerd Font:h8'
+    -- vim.o.guifont = 'JetBrainsMono Nerd Font:h11'
+    vim.g.neovide_text_gamma = 1
+    vim.g.neovide_text_contrast = 1
+    vim.g.neovide_input_use_logo = true
+    -- vim.g.neovide_refresh_rate = 165
+    vim.g.neovide_refresh_rate_idle = 15
+    vim.g.neovide_opacity = 0.8
+    vim.g.transparency = 1
+    vim.opt.linespace = 2
+    vim.g.neovide_floating_corner_radius = 0.5
+
+    self.set_env()
+    self.set_keymaps()
+    vim.keymap.set('n', '<C-)>', function() self:set_transparency(0.05) end)
+    vim.keymap.set('n', '<C-(>', function() self:set_transparency(-0.05) end)
 end
 
 return M
