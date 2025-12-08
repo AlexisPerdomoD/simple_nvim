4716fbe2562ed501d0bfc180c0a3a7266dcedd12
cuji <alexmetal66@gmail.com>
2025-12-08 17:55:30 -0500

    feat: actualizar script de instalacion de entorno de desarrollo

--- install.sh
+++ install.sh
@@ -1,1 +1,9 @@
-sudo pacman -S --needed --noconfirm git curl neovim ripgrep fd bat fzf go dotnet-sdk nodejs
+sudo pacman -S --needed --noconfirm git curl neovim ripgrep fd bat fzf go nodejs fnm neovide pnpm
+
+yay -S --needed --noconfirm sdkman
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+############# Java #################
+sdk install maven
+sdk install java 15.0.12-tem
+sdk install java 17.0.12-tem
+sdk install java 21.0.05-tem
