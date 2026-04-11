43c492cd343a99f76a3521aea6d7be894a24d2a6
perdomo <dropdibemol@gmail.com>
2026-04-11 16:44:48 -0500

    fix: clean unnused code lsp related

--- lsp_config_to_ckeck /css_modules_ls.lua
+++
@@ -1,5 +0,0 @@
-return {
-    cmd = { 'cssmodules-language-server', '--stdio' },
-    filetypes = { 'html', 'vue', 'css', 'scss', 'less', 'svelte' },
-    root_markers = { 'package.json', '.git' },
-}
