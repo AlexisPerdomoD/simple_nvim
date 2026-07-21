372e390f409ffc4bcb4a72de9b570c62ae5856c9
perdomo <perdomo@local>
2026-07-21 14:24:27 -0500

    refactor(init): load opts/autocmds/keymaps before neovide config

--- init.lua
+++ init.lua
@@ -1,9 +1,10 @@
+require 'config.opts'
+require 'config.autocmds'
+require 'config.keymaps'
+
 if vim.g.neovide then
     local cfg = require 'config.neovide'
     cfg:setup()
 end
 
-require 'config.opts'
-require 'config.autocmds'
-require 'config.keymaps'
 require 'config.lazy'
